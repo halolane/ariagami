884814b84fbe9ed6371bcf44c989a5bba9f78104
halolane <myhalolane@gmail.com>
2013-09-11 14:14:12 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ariagami::Application.config.secret_token = '0070c9ae1785d841c714672a77efe836555c252cb5f2a1120007b3464871a4452b347b5d8e6e18978699636cf03e887a2824ae673db2ca149702e0d6d8159bfe'
